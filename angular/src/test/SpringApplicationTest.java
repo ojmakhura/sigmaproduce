59754130ee44daa559d9300b038daf5bd864401f
Onalenna Makhura <ojmakhura@gmail.com>
2024-05-11 18:49:06 +0200

    added angular files

---
+++ angular/src/test/SpringApplicationTest.java
@@ -0,0 +1,13 @@
+package bw.co.roguesystems.sigma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SigmaproduceAngularApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
